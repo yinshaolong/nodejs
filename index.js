5b6fc221922f30e10cf9ec1ea78173eccaa41ef7
omgitsjohn <syin18@my.bcit.ca>
2023-12-05 22:21:51 -0800

    writeFileSync - takes two args, path & data

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const fs = require('fs'); //gives access to the file system
 
 const textIn = fs.readFileSync('./txt/input.txt', "utf-8") //takes two arguments, the path to the file and the character encoding to be used to read the file
 console.log(textIn);
+
+const textOut =`This is what we know abot the avocade: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut); //takes two arguments, the path to the file and the data to be written to the file
+console.log('File written!');
