fcecc88850b78528f1a04ab790fc3555e595ab80
omgitsjohn <syin18@my.bcit.ca>
2023-12-06 22:31:52 -0800

    creating a local web server with http module

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require('fs'); //gives access to the file system
+const http = require('http'); // gives networking capabilities i.e. building a n http server
 
+//################### FILES ###################
 
 function fileSync(){
     const textIn = fs.readFileSync('./txt/input.txt', "utf-8") //takes two arguments, the path to the file and the character encoding to be used to read the file
@@ -15,9 +17,9 @@ function fileAsync(){
         if (err) return console.log("ERROR! 💥");
         fs.readFile(`./txt/${file_content}.txt`, "utf-8", (err, data) => {
             console.log(data);
-
+            
             fs.readFile(`./txt/append.txt`, "utf-8", (err, data2) => {
-            console.log(data2)
+                console.log(data2)
                 //nested another readFile and a writeFile inside that has access to data and data2
                 fs.writeFile("./txt/final.txt", `${data}\n${data2}`, "utf-8", err => {
                     console.log("Your file has been written");
@@ -28,5 +30,17 @@ function fileAsync(){
     console.log("Will read file! This first");
 };
 
+//################### SERVER ###################
+
+const server = http.createServer((request, response)=>{  //takes callback function as an argument -> request and response
+    response.end("hello from the server"); //sends a response to the client
+})
+
+//starts the server and listens to incoming requests
+server.listen(8000, '127.0.0.1', () =>{
+    console.log("Listening to requests on port 8000");
+}) //takes port, host (in this case the local host), callback function
+
+
 // fileSync();
-fileAsync();
+fileAsync(); 
